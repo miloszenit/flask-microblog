6bc2e98cc8943e0aafb5c16febcf4756872f572f
miloszenit <xymilos@gmail.com>
2022-09-25 14:01:00 +0200

    Fix the duplicate username bug

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, \
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
     TextAreaField
 from app.models import User
+from flask_login import current_user
 
 
 class LoginForm(FlaskForm):
@@ -36,3 +37,9 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user:
+                raise ValidationError('Please use a different username.')
